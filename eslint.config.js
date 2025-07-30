95c2cdfaf7d68fd05a4635fa14b56e943e283cfd
zhangshaofan <zhangshaofan@360.cn>
2025-07-30 15:21:18 +0800

    feat: husky

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "node_modules/**",
+      "packages/**/dist/**",
+      "packages/**/node_modules/**",
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+      sourceType: "module",
+    },
+  },
+  {
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+    },
+  },
+);
